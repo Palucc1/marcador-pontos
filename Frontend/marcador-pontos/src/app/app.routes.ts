69824787cb483023e1e880185df30cea5171cd2e
Guilherme André Palucci <guipalucci@outlook.com>
2025-01-22 10:54:13 -0300

    feat(frontend): criacao do projeto no frontend em angular e estruturacao das pastas da aplicacao.

---
+++ Frontend/marcador-pontos/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home/home.component';
+import { CadastroPartidaComponent } from '../pages/cadastro-partida/cadastro-partida.component';
+import { ResultadosComponent } from '../pages/resultados/resultados.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    { path: 'cadastro', component: CadastroPartidaComponent},
+    { path: 'resultados', component: ResultadosComponent},
+];
---
+++ Frontend/marcador-pontos/src/pages/cadastro-partida/cadastro-partida.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-partida',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-partida.component.html',
+  styleUrl: './cadastro-partida.component.scss'
+})
+export class CadastroPartidaComponent {
+
+}
